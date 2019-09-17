4583dc5b9dd0fbd622457fc36263079840fb3741
Tri M. Nguyen <tri_nguyen@hms.harvard.edu>
2019-09-17 14:07:30 -0400

    Remove myelin_postprocess from __init__
    To reduce import latency

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from .tasks import task_helper as task_helper
 from .tasks import segmentation_functions as segmentation_functions
-from .myelin_scripts.myelin_postprocess_pipeline_setup00 import run_postprocess_setup as myelin_postprocess
+# from .myelin_scripts.myelin_postprocess_pipeline_setup00 import run_postprocess_setup as myelin_postprocess
+from .gt_scripts import gt_tools as gt_tools
 
-__all__ = ["task_helper", "segmentation_functions", "myelin_postprocess"]
+# __all__ = ["task_helper", "segmentation_functions", "myelin_postprocess", "gt_tools"]
+__all__ = ["task_helper", "segmentation_functions", "gt_tools"]
