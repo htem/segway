695c34e689692811de55af84a8def563a81a2de3
Tri M. Nguyen <tri_nguyen@hms.harvard.edu>
2019-09-23 11:06:09 -0400

    Clean up

--- tasks/task_05_extract_segmentation_from_lut_blockwise.py
+++ tasks/task_05_extract_segmentation_from_lut_blockwise.py
@@ -1,16 +1,16 @@
 import logging
-import lsd
+# import lsd
 # import numpy as np
 import daisy
-from daisy import Coordinate, Roi
+# from daisy import Coordinate, Roi
 import sys
 import numpy as np
 
-import os
+# import os
 
 # from funlib.segment.arrays import replace_values
 
-import task_helper
+import task_helper2 as task_helper
 from task_04d_find_segment_blockwise import FindSegmentsBlockwiseTask4
 
 # logging.getLogger('lsd.parallel_fragments').setLevel(logging.DEBUG)
